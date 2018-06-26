628bd64fc6839713d12273ea0d46cb3e7c48c8e2
Ricky <rickynyairo@gmail.com>
2018-06-26 10:59:25 +0300

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,4 +1,11 @@
 $(function(){
+    var colour = $("#colorPicker").val()
+    
+    //event listener for colour selection
+    $("#colorPicker").on("input", function(){            
+	colour = $(this).val();
+	console.log(`Colour: ${colour}`);
+    });
     //event listener for submit action.
     $("#sizePicker").submit(function(evt){
         evt.preventDefault();
@@ -6,13 +13,10 @@ $(function(){
         var height = Number($("#inputHeight").val());
         var width = Number($("#inputWeight").val());
         makeGrid(height, width);
-        var colour = "#000000";
-        //event listener for colour selection
-        $("#colorPicker").on("input", function(){            
-            colour = $(this).val();
-            console.log(`Colour: ${colour}`);
-        });
+	    
         //event listener for each click action
+	//the event listener is placed within the callback function of the submit action
+	//to ensure the table is created before the event listener.
         $("tr.tableRow td").click(function(){  
             $(this).css("background-color", colour);
         });
@@ -21,9 +25,9 @@ $(function(){
 function makeGrid(height, width) {
     table = $("#pixelCanvas");
     for (var i = 0; i < height; i++) {
-		var row = $('<tr></tr>').attr("class", "tableRow").appendTo(table);
-		for (var j = 0; j < width; j++) {
-			$('<td></td>').appendTo(row); 
-		}		 		 
-	}
+	var row = $('<tr></tr>').attr("class", "tableRow").appendTo(table);
+	for (var j = 0; j < width; j++) {
+	    $('<td></td>').appendTo(row); 
+	}		 		 
+    }
 }
